0936b07226c49d98e48ceb35eac80958a69b4328
Langdon Douglas <langdon.douglas26@gmail.com>
2024-09-03 15:55:57 -0500

    working on resize Function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector("#container");
-const alertButton = document.querySelector("#alert");
+const resizeButton = document.querySelector("#resize");
+alertButton.textContent = "Click Here";
 
 const gridSize = 16;
 
@@ -13,5 +14,27 @@ function createGrid(size){
         container.appendChild(square);
     }
 }
+
+function resizeGrid(){
+    let newGridSize = parseInt(prompt("What's the number of squares per side?", "16"));
+   
+    for (let i = 0; i < 100; i++){
+        
+        if(isNaN(newGridSize) || newGridSize <= 0){
+            alert("Number must greater than 0");
+            return;
+        }
+
+        const newContainerSize = 960;
+        const squareSize = newContainerSize / newGridSize;
+        const css = document.styleSheets[0];
+        css.insertRule(`.grid-square { flex: 0 0 ${100 / newGridSize}%; height: ${squareSize}px; }`, css.cssRules.length);
+    }
+        
+    createGrid(newGridSize);
+
+}
+
+resizeButton.addEventListener("click", resizeGrid);
 
 createGrid(gridSize);
