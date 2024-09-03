61873527f23a04ce947fe2a7684a60ed198a3c79
Langdon Douglas <langdon.douglas26@gmail.com>
2024-09-03 15:22:17 -0500

    CSS change to square's borders / Added method to display grid elements

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector("#container");
+
+const gridSize = 16;
+
+function createGrid(size){
+    const totalSquares = size * size;
+    
+    for (let i = 0; i < totalSquares; i++){
+        const square = document.createElement("div");
+        square.classList.add("grid-square");
+        console.log(square)
+        container.appendChild(square);
+    }
+}
+
+createGrid(gridSize);
